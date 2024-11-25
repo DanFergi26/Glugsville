1c1e9c11b2ff36f6f46f91222d17433f28a0aacd
SweetPrince747 <40534169@live.napier.ac.uk>
2024-11-25 18:37:17 +0000

    all basic levels added

--- lab_7_platformer/scenes/scene_level3.cpp
+++ lab_7_platformer/scenes/scene_level3.cpp
@@ -11,29 +11,23 @@ using namespace sf;
 static shared_ptr<Entity> player;
 
 void Level3Scene::Load() {
-  cout << "Scene 3 Load" << endl;
-  ls::loadLevelFile("res/level_3.txt", 40.0f);
-  auto ho = Engine::getWindowSize().y - (ls::getHeight() * 40.f);
-  ls::setOffset(Vector2f(0, ho));
+    cout << "Scene 3 Load" << endl;
+    ls::loadLevelFile("res/level_3.txt", 40.0f);
+    auto ho = Engine::getWindowSize().y - (ls::getHeight() * 40.f);
+    ls::setOffset(Vector2f(0, ho));
 
-  // Create player
-  {
-    // *********************************
+    // Create player
+    {
+        // *********************************
 
+        player = makeEntity();
+        player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
+        auto s = player->addComponent<ShapeComponent>();
+        s->setShape<sf::RectangleShape>(Vector2f(20.f, 30.f));
+        s->getShape().setFillColor(Color::Magenta);
+        s->getShape().setOrigin(Vector2f(10.f, 15.f));
 
-    // pl->setPosition({100, 100});
-
-
-
-
-
-
-    // *********************************
-  }
-
-  // Add physics colliders to level tiles.
-  {
-    // *********************************
+        player->addComponent<PlayerPhysicsComponent>(Vector2f(20.f, 30.f));
 
 
 
@@ -42,54 +36,89 @@ void Level3Scene::Load() {
 
 
 
-    // *********************************
-  }
 
-  cout << " Scene 3 Load Done" << endl;
-  setLoaded(true);
+        // *********************************
+    }
+
+    // Add physics colliders to level tiles.
+    {
+        // *********************************
+
+        {
+            auto walls = ls::findTiles(ls::WALL);
+            for (auto w : walls) {
+                auto pos = ls::getTilePosition(w);
+                pos += Vector2f(20.f, 20.f); //offset to center
+                auto e = makeEntity();
+                e->setPosition(pos);
+                e->addComponent<PhysicsComponent>(false, Vector2f(40.f, 40.f));
+            }
+        }
+
+        // *********************************
+    }
+
+    cout << " Scene 3 Load Done" << endl;
+    setLoaded(true);
 }
 
 void Level3Scene::UnLoad() {
-  cout << "Scene 3 UnLoad" << endl;
-  player.reset();
-  ls::unload();
-  Scene::UnLoad();
+    cout << "Scene 3 UnLoad" << endl;
+    player.reset();
+    ls::unload();
+    Scene::UnLoad();
 }
 
 
 
 void Level3Scene::Update(const double& dt) {
-  Scene::Update(dt);
-  const auto pp = player->getPosition();
-  if (ls::getTileAt(pp) == ls::END) {
-    Engine::ChangeScene((Scene*)&level1);
-  } else if (!player->isAlive()) {
-    Engine::ChangeScene((Scene*)&level3);
-  }
+    // Center the view on the player, with constraints
+    auto windowSize = Engine::getWindowSize();
+    auto levelWidth = ls::getWidth() * 40.f;
 
-  static float rocktime = 0.0f;
-  rocktime -= dt;
+    Vector2f viewCenter = player->getPosition();
+    viewCenter.y = windowSize.y / 2.0f; // Center vertically
+    viewCenter.x = std::max(windowSize.x / 2.0f, viewCenter.x); // Prevent scrolling left out of bounds
+    viewCenter.x = std::min(levelWidth - windowSize.x / 2.0f, viewCenter.x); // Prevent scrolling past right boundary
 
-  if (rocktime <= 0.f){
-    rocktime  = 5.f;
-    auto rock = makeEntity();
-    rock->setPosition(ls::getTilePosition(ls::findTiles('r')[0]) +
-                      Vector2f(0, 40) );
-    rock->addComponent<BulletComponent>(30.f);
-    auto s = rock->addComponent<ShapeComponent>();
-    s->setShape<sf::CircleShape>(40.f);
-    s->getShape().setFillColor(Color::Cyan);
-    s->getShape().setOrigin(Vector2f(40.f, 40.f));
-    auto p = rock->addComponent<PhysicsComponent>(true, Vector2f(75.f, 75.f));
-    p->setRestitution(.4f);
-    p->setFriction(.0001f);
-    p->impulse(Vector2f(-3.f, 0));
-    p->setMass(1000000000.f);
-  }
-  
+    // Set the new view
+    sf::View view;
+    view.setSize(Vector2f(windowSize.x, windowSize.y));
+    view.setCenter(viewCenter);
+    Engine::GetWindow().setView(view);
+
+    Scene::Update(dt);
+    const auto pp = player->getPosition();
+    if (ls::getTileAt(pp) == ls::END) {
+        Engine::ChangeScene((Scene*)&level1);
+    }
+    else if (!player->isAlive()) {
+        Engine::ChangeScene((Scene*)&level3);
+    }
+
+    static float rocktime = 0.0f;
+    rocktime -= dt;
+
+    if (rocktime <= 0.f) {
+        rocktime = 5.f;
+        auto rock = makeEntity();
+        rock->setPosition(ls::getTilePosition(ls::findTiles('r')[0]) +
+            Vector2f(0, 40));
+        rock->addComponent<BulletComponent>(30.f);
+        auto s = rock->addComponent<ShapeComponent>();
+        s->setShape<sf::CircleShape>(40.f);
+        s->getShape().setFillColor(Color::Cyan);
+        s->getShape().setOrigin(Vector2f(40.f, 40.f));
+        auto p = rock->addComponent<PhysicsComponent>(true, Vector2f(75.f, 75.f));
+        p->setRestitution(.4f);
+        p->setFriction(.0001f);
+        p->impulse(Vector2f(-3.f, 0));
+        p->setMass(1000000000.f);
+    }
+
 }
 
 void Level3Scene::Render() {
-  ls::render(Engine::GetWindow());
-  Scene::Render();
+    ls::render(Engine::GetWindow());
+    Scene::Render();
 }
